f45114b6eced8ce6a8cdcebe96e8dd79c49b034f
saidone <saidone@saidone.org>
2019-08-05 08:47:06 +0200

    single point config for Alfresco download URL

--- vagralf.sh
+++ vagralf.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
 
+ALF_DOWNLOAD_URL=https://download.alfresco.com/cloudfront/release/community/201901-GA-build-205/alfresco-content-services-community-distribution-6.1.2-ga.zip
+LOGFILE=/vagrant/vagralf.log
+
 IFS=$'\n'
 
 DEBUG=1
 COLOUR=1
 
-LOGFILE=/vagrant/vagralf.log
+export ALF_ZIP=$(sed s/^.*[\/]// <<< $ALF_DOWNLOAD_URL)
+export ALF_DIR=$(sed s/\.[^\.]*$// <<< $ALF_ZIP)
 
 function info() {
-	for LINE in $1; do
-		if [ $COLOUR -eq 0 ]; then
-			echo -e "[INFO] $LINE"
-		else
-			echo -e "\t\033[0;32m[INFO]\033[0;370m $LINE"
-		fi
-	done
+	  for LINE in $1; do
+		    if [ $COLOUR -eq 0 ]; then
+			      echo -e "[I] $LINE"
+		    else
+			      echo -e "\t\033[0;32m[I]\033[0;370m $LINE"
+		    fi
+	  done
 }
 
 function debug() {
-	for LINE in $1; do
-		if [ $DEBUG -eq 1 ]; then
-			if [ $COLOUR -eq 0 ]; then
-				echo -e "[DEBUG] $LINE"
-			else
-				echo -ne "\t\033[0;33m[DEBUG]\033[0;370m $LINE"
-			fi
-		fi
-	done
+	  for LINE in $1; do
+		    if [ $DEBUG -eq 1 ]; then
+			      if [ $COLOUR -eq 0 ]; then
+				        echo -e "[D] $LINE"
+			      else
+				        echo -ne "\t\033[0;33m[D]\033[0;370m $LINE"
+			      fi
+		    fi
+	  done
 }
 
 function fatal() {
-	for LINE in $1; do
-		if [ $COLOUR -eq 0 ]; then
-			echo -e "[FATAL] $LINE"
-		else
-			echo -e "\t\033[0;31m[FATAL]\033[0;370m $LINE"
-		fi
-		exit 1
-	done
+	  for LINE in $1; do
+		    if [ $COLOUR -eq 0 ]; then
+			      echo -e "[F] $LINE"
+		    else
+			      echo -e "\t\033[0;31m[F]\033[0;370m $LINE"
+		    fi
+		    exit 1
+	  done
 }
 
 # update list of available packages
@@ -51,37 +55,36 @@ apt install openjdk-8-jre-headless tomcat7 postgresql unzip imagemagick --assume
 cd /vagrant
 # download and unzip Alfresco community distribution
 info "downloading Alfresco community distribution"
-if [ ! -f alfresco-content-services-community-distribution-6.1.2-ga.zip ]; then
-    wget https://download.alfresco.com/cloudfront/release/community/201901-GA-build-205/alfresco-content-services-community-distribution-6.1.2-ga.zip &>> $LOGFILE
+if [ ! -f $ALF_ZIP ]; then
+    wget $ALF_DOWNLOAD_URL &>> $LOGFILE
 fi
-unzip alfresco-content-services-community-distribution-6.1.2-ga.zip &>> $LOGFILE
-export ALFRESCO=alfresco-content-services-community-distribution-6.1.2-ga
+unzip $ALF_ZIP &>> $LOGFILE
 
 info "configuring Tomcat and Alfresco"
 # stop Tomcat
 service tomcat7 stop &
 
 # Tomcat config
-cp $ALFRESCO/web-server/conf/Catalina/localhost/* /etc/tomcat7/Catalina/localhost
+cp $ALF_DIR/web-server/conf/Catalina/localhost/* /etc/tomcat7/Catalina/localhost
 
 # PostgreSQL JDBC driver
-cp $ALFRESCO/web-server/lib/* /usr/share/tomcat7/lib
+cp $ALF_DIR/web-server/lib/* /usr/share/tomcat7/lib
 
 # install Share Services AMP
-java -jar $ALFRESCO/bin/alfresco-mmt.jar install $ALFRESCO/amps/* $ALFRESCO/web-server/webapps/alfresco.war
+java -jar $ALF_DIR/bin/alfresco-mmt.jar install $ALF_DIR/amps/* $ALF_DIR/web-server/webapps/alfresco.war
 
 # Alfresco and Share WARs
-rm -rf /var/lib/tomcat7/work/Catalina/localhost/* 
+rm -rf /var/lib/tomcat7/work/Catalina/*
 rm -rf /var/lib/tomcat7/webapps/*
-cp $ALFRESCO/web-server/webapps/{alfresco,share}.war /var/lib/tomcat7/webapps/
+cp $ALF_DIR/web-server/webapps/{alfresco,share}.war /var/lib/tomcat7/webapps/
 
 # Alfresco config       
-cp -r $ALFRESCO/web-server/shared /usr/share/tomcat7/shared
+cp -r $ALF_DIR/web-server/shared /usr/share/tomcat7/shared
 cp alfresco-global.properties /usr/share/tomcat7/shared/classes/
 
 # create alf_data and setting ownership
 mkdir -p /srv/alfresco
-cp -R $ALFRESCO/alf_data /srv/alfresco
+cp -R $ALF_DIR/alf_data /srv/alfresco
 chown -R tomcat7:tomcat7 /srv/alfresco
 
 # create Alfresco database and user
@@ -99,8 +102,7 @@ cat /dev/null > /var/log/tomcat7/catalina.out
 service tomcat7 start &
 
 # install alfresco-pdf-renderer
-info "installing alfresco-pdf-renderer"
-tar -xzf $ALFRESCO/alfresco-pdf-renderer/alfresco-pdf-renderer-1.1-linux.tgz -C /usr/bin/
+tar -xzf $ALF_DIR/alfresco-pdf-renderer/alfresco-pdf-renderer-1.1-linux.tgz -C /usr/bin/
 
 # wait for Alfresco deployment
 info "waiting for Alfresco deployment"
@@ -108,4 +110,4 @@ while ! sudo grep "INFO: Server startup in" /var/log/tomcat7/catalina.out; do sl
 info "done!"
 
 # remove unzipped directory, leave zip archive
-rm -rf alfresco-content-services-community-distribution-6.1.2-ga
+rm -rf $ALF_DIR
