89f1e2041bbcf588e07927c472891b1f9674bb06
saidone <saidone@saidone.org>
2020-01-23 10:32:54 +0100

    new release for alfresco-content-services-community-distribution-6.2.0-ga

--- vagralf.sh
+++ vagralf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ALF_DOWNLOAD_URL=https://download.alfresco.com/cloudfront/release/community/201901-GA-build-205/alfresco-content-services-community-distribution-6.1.2-ga.zip
+ALF_DOWNLOAD_URL=https://download.alfresco.com/cloudfront/release/community/201911-GA-build-368/alfresco-content-services-community-distribution-6.2.0-ga.zip
 LOGFILE=/vagrant/vagralf.log
 
 IFS=$'\n'
@@ -50,7 +50,7 @@ apt update &> $LOGFILE
 
 # install required packages
 info "installing required packages"
-apt install openjdk-8-jre-headless tomcat7 postgresql unzip imagemagick --assume-yes &>> $LOGFILE
+apt install openjdk-11-jre-headless tomcat8 postgresql unzip imagemagick --assume-yes &>> $LOGFILE
 
 cd /vagrant
 # download and unzip Alfresco community distribution
@@ -62,30 +62,31 @@ unzip $ALF_ZIP &>> $LOGFILE
 
 info "configuring Tomcat and Alfresco"
 # stop Tomcat
-service tomcat7 stop &
+service tomcat8 stop &
 
 # Tomcat config
-cp $ALF_DIR/web-server/conf/Catalina/localhost/* /etc/tomcat7/Catalina/localhost
+sed '/<Resources/,/Resources>/d' $ALF_DIR/web-server/conf/Catalina/localhost/share.xml | sudo tee /etc/tomcat8/Catalina/localhost/share.xml &>> $LOGFILE
+sed '/<Resources/,/Resources>/d' $ALF_DIR/web-server/conf/Catalina/localhost/alfresco.xml | sudo tee /etc/tomcat8/Catalina/localhost/alfresco.xml &>> $LOGFILE
 
 # PostgreSQL JDBC driver
-cp $ALF_DIR/web-server/lib/* /usr/share/tomcat7/lib
+cp $ALF_DIR/web-server/lib/* /usr/share/tomcat8/lib
 
 # install Share Services AMP
 java -jar $ALF_DIR/bin/alfresco-mmt.jar install $ALF_DIR/amps/* $ALF_DIR/web-server/webapps/alfresco.war
 
 # Alfresco and Share WARs
-rm -rf /var/lib/tomcat7/work/Catalina/*
-rm -rf /var/lib/tomcat7/webapps/*
-cp $ALF_DIR/web-server/webapps/{alfresco,share}.war /var/lib/tomcat7/webapps/
+rm -rf /var/lib/tomcat8/work/Catalina/*
+rm -rf /var/lib/tomcat8/webapps/*
+cp $ALF_DIR/web-server/webapps/{alfresco,share}.war /var/lib/tomcat8/webapps/
 
 # Alfresco config       
-cp -r $ALF_DIR/web-server/shared /usr/share/tomcat7/shared
-cp alfresco-global.properties /usr/share/tomcat7/shared/classes/
+cp -r $ALF_DIR/web-server/shared /usr/share/tomcat8/shared
+cp alfresco-global.properties /usr/share/tomcat8/shared/classes/
 
 # create alf_data and setting ownership
 mkdir -p /srv/alfresco
 cp -R $ALF_DIR/alf_data /srv/alfresco
-chown -R tomcat7:tomcat7 /srv/alfresco
+chown -R tomcat8:tomcat8 /srv/alfresco
 
 # create Alfresco database and user
 info "creating Alfresco database and user"
@@ -94,19 +95,19 @@ sudo -u postgres bash -c "psql -c \"CREATE USER alfresco WITH PASSWORD 'alfresco
 sudo -u postgres bash -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE alfresco TO alfresco;\"" &>> $LOGFILE
 
 # increase Tomcat memory limit...
-sed '/^#!\/bin\/sh/a JAVA_OPTS="-Xms2048m -Xmx2048m"' /usr/share/tomcat7/bin/catalina.sh | sudo tee /usr/share/tomcat7/bin/catalina.sh &>> $LOGFILE
+sed '/^#!\/bin\/sh/a JAVA_OPTS="-Xms2048m -Xmx2048m"' /usr/share/tomcat8/bin/catalina.sh | sudo tee /usr/share/tomcat8/bin/catalina.sh &>> $LOGFILE
 
 # ...and restart Tomcat!
 info "starting Tomcat"
-cat /dev/null > /var/log/tomcat7/catalina.out
-service tomcat7 start &
+cat /dev/null > /var/log/tomcat8/catalina.out
+service tomcat8 start &
 
 # install alfresco-pdf-renderer
 tar -xzf $ALF_DIR/alfresco-pdf-renderer/alfresco-pdf-renderer-1.1-linux.tgz -C /usr/bin/
 
 # wait for Alfresco deployment
 info "waiting for Alfresco deployment"
-while ! sudo grep "INFO: Server startup in" /var/log/tomcat7/catalina.out; do sleep 5; done
+while ! sudo grep "Alfresco Content Services started" /var/log/tomcat8/catalina.out; do sleep 5; done
 info "done!"
 
 # remove unzipped directory, leave zip archive
